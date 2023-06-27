0bd53f1e63a1cc1f16c4d881cc4e1f7354d54ef8
Fabian <fabianrincon1407@gmail.com>
2023-06-27 13:26:22 -0500

    solucion del problema en la consulta para buscar HU por equipo y se elimino una interfaz llamada boardRepositoryCustom que no estaba siendo utilizada

--- src/main/java/com/wposs/scrum_back/userstory/service/UserStoryServiceImpl.java
+++ src/main/java/com/wposs/scrum_back/userstory/service/UserStoryServiceImpl.java
@@ -9,6 +9,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -69,6 +71,7 @@ public class UserStoryServiceImpl implements UserStoryService{
     }
 
     @Override
+    @Transactional
     public List<UserStoryDto> getAllUserStoryToTeam(UUID idTeam) {
         return userStoryRepository.getAllSserStoryToTeam(idTeam)
                 .stream()
