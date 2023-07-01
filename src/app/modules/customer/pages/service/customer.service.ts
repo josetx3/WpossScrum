66d6cbe9a7187545c7352d1a9d7835a983fab267
Fabian <fabianrincon1407@gmail.com>
2023-07-01 08:43:15 -0500

    Configuracion del envio del token de autorizacion en cada peticion al back

--- src/app/modules/customer/pages/service/customer.service.ts
+++ src/app/modules/customer/pages/service/customer.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -13,19 +13,27 @@ export class CustomerService {
   }
 
   public getAllCustomer(): Observable<any> {
-    return this.httpClient.get(this.API_SERVER + "/all");
+    const token: string | null= ''+localStorage.getItem('token')
+    const headers = new HttpHeaders().set("Authorization", token);
+    return this.httpClient.get(this.API_SERVER + "/all",{headers});
   }
 
   public saveCustomer(customer: any): Observable<any> {
-    return this.httpClient.post(this.API_SERVER + "/save/", customer);
+    const token: string | null= ''+localStorage.getItem('token')
+    const headers = new HttpHeaders().set("Authorization", token);
+    return this.httpClient.post(this.API_SERVER + "/save/", customer,{headers});
   }
 
   public getCustomerById(id: string | null): Observable<any> {
-    return this.httpClient.get(this.API_SERVER + "/" + id);
+    const token: string | null= ''+localStorage.getItem('token')
+    const headers = new HttpHeaders().set("Authorization", token);
+    return this.httpClient.get(this.API_SERVER + "/" + id,{headers});
   }
 
   public updateCustomer(id: string, dataCustomer: any) {
-    return this.httpClient.put(this.API_SERVER + "/" + id, dataCustomer);
+    const token: string | null= ''+localStorage.getItem('token')
+    const headers = new HttpHeaders().set("Authorization", token);
+    return this.httpClient.put(this.API_SERVER + "/" + id, dataCustomer,{headers});
   }
 
 }
