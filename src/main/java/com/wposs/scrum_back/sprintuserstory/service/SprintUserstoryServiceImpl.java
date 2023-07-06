c4baaf1b340a97ffaff001e3d2b74884e804f7b7
Fabian <fabianrincon1407@gmail.com>
2023-07-06 16:49:39 -0500

    Se realiza elk cambio de la historia de usuario de refinada a desarrollo

--- src/main/java/com/wposs/scrum_back/sprintuserstory/service/SprintUserstoryServiceImpl.java
+++ src/main/java/com/wposs/scrum_back/sprintuserstory/service/SprintUserstoryServiceImpl.java
@@ -9,21 +9,41 @@ import com.wposs.scrum_back.sprintuserstory.dto.SprintUserstoryDto;
 import com.wposs.scrum_back.sprintuserstory.entity.SprintUserstory;
 import com.wposs.scrum_back.sprintuserstory.entity.SprintUserstoryPk;
 import com.wposs.scrum_back.sprintuserstory.repository.SprintUserstoryRepository;
+import com.wposs.scrum_back.userstory.entity.UserStory;
+import com.wposs.scrum_back.userstory.repository.UserStoryRepository;
+import com.wposs.scrum_back.userstorystatus.entity.UserStoryStatus;
+import com.wposs.scrum_back.userstorystatus.repository.UserStoryStatusRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class SprintUserstoryServiceImpl implements SprintUserstoryService {
     @Autowired
     private SprintUserstoryRepository sprintUserstoryRepository;
+
+    @Autowired
+    private UserStoryRepository userStoryRepository;
+
+    @Autowired
+    private UserStoryStatusRepository userStoryStatusRepository;
     @Autowired
     private ModelMapper modelMapper;
     @Override
     public SprintUserstoryDto saveSprintUserStory(SprintUserstoryDto sprintUserstoryDto) {
+
         SprintUserstory sprintUserstory = modelMapper.map(sprintUserstoryDto,SprintUserstory.class);
-        SprintUserstoryPk sprintUserstoryPk = new SprintUserstoryPk(sprintUserstory.getId().getUserStoryId(),sprintUserstory.getId().getSprintId());
+
+       Optional<UserStory> userStory= userStoryRepository.findById(sprintUserstory.getId().getUserStoryId());
+       UserStoryStatus userStoryStatus = userStoryStatusRepository.findByUserStoryStateName("DESARROLLO");
+       Long p= Long.valueOf(userStoryStatus.getUserStoryStateId());
+       //
+       userStory.get().setUserStoryStateId(p);
+       userStoryRepository.save(userStory.get());
+       SprintUserstoryPk sprintUserstoryPk = new SprintUserstoryPk(sprintUserstory.getId().getUserStoryId(),sprintUserstory.getId().getSprintId());
         if (sprintUserstoryRepository.existsById(sprintUserstoryPk)){
             throw new MessageGeneric("","", HttpStatus.CONFLICT);
         }
--- src/main/java/com/wposs/scrum_back/userstorystatus/repository/UserStoryStatusRepository.java
+++ src/main/java/com/wposs/scrum_back/userstorystatus/repository/UserStoryStatusRepository.java
@@ -11,6 +11,9 @@ import java.util.List;
 @Repository
 public interface UserStoryStatusRepository extends JpaRepository<UserStoryStatus,Long> {
     Boolean existsByUserStoryStateName(String stateName);
+
+
+    UserStoryStatus findByUserStoryStateName(String stateName);
     @Query(value = "SELECT * FROM wposs.user_story_status",nativeQuery = true)
     List<UserStoryStatus> getAllStatus();
 }
